754137718b695e25225fd32b9fec85f5033d718b
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2022-06-25 06:40:39 -0700

    refactor: update prune-data and tests to be more generic and modern

--- external-scripts/prune-data.ts
+++ external-scripts/prune-data.ts
@@ -4,13 +4,24 @@ import { AppError, InvalidAppEnvironmentError } from 'named-app-errors';
 import { getDb } from 'multiverse/mongo-schema';
 import { toss } from 'toss-expression';
 import { debugFactory } from 'multiverse/debug-extended';
+import { deleteUser } from 'universe/backend';
+
+import type { Document, WithId } from 'mongodb';
+import type { Promisable } from 'type-fest';
+import type { InternalNode, InternalUser } from 'universe/backend/db';
 
 const debugNamespace = `${namespace}:prune-data`;
 
 const log = debugFactory(debugNamespace);
 const debug = debugFactory(debugNamespace);
 
-type DataLimit = number | { limit: number; orderBy: string };
+type DataLimit =
+  | number
+  | {
+      limit: number;
+      orderBy?: string;
+      deleteFn?: (thresholdEntry: WithId<Document>) => Promisable<number>;
+    };
 
 // eslint-disable-next-line no-console
 log.log = console.info.bind(console);
@@ -20,24 +31,106 @@ if (!getEnv().DEBUG && getEnv().NODE_ENV != 'test') {
   debug.enabled = false;
 }
 
-const getCollectionLimits = (env: ReturnType<typeof getEnv>) => {
-  const limits: Record<string, DataLimit> = {
-    'request-log':
-      env.PRUNE_DATA_MAX_LOGS && env.PRUNE_DATA_MAX_LOGS > 0
-        ? env.PRUNE_DATA_MAX_LOGS
-        : toss(
-            new InvalidAppEnvironmentError(
-              'PRUNE_DATA_MAX_LOGS must be greater than zero'
+// * Add new env var configurations here
+const getDbCollectionLimits = (env: ReturnType<typeof getEnv>) => {
+  const limits: Record<string, Record<string, DataLimit>> = {
+    root: {
+      'request-log':
+        env.PRUNE_DATA_MAX_LOGS && env.PRUNE_DATA_MAX_LOGS > 0
+          ? env.PRUNE_DATA_MAX_LOGS
+          : toss(
+              new InvalidAppEnvironmentError(
+                'PRUNE_DATA_MAX_LOGS must be greater than zero'
+              )
+            ),
+      'limited-log':
+        env.PRUNE_DATA_MAX_BANNED && env.PRUNE_DATA_MAX_BANNED > 0
+          ? env.PRUNE_DATA_MAX_BANNED
+          : toss(
+              new InvalidAppEnvironmentError(
+                'PRUNE_DATA_MAX_BANNED must be greater than zero'
+              )
             )
-          ),
-    'limited-log':
-      env.PRUNE_DATA_MAX_BANNED && env.PRUNE_DATA_MAX_BANNED > 0
-        ? env.PRUNE_DATA_MAX_BANNED
-        : toss(
-            new InvalidAppEnvironmentError(
-              'PRUNE_DATA_MAX_BANNED must be greater than zero'
+    },
+    'hscc-api-drive': {
+      'file-nodes': {
+        limit:
+          env.PRUNE_DATA_MAX_FILE_NODES && env.PRUNE_DATA_MAX_FILE_NODES > 0
+            ? env.PRUNE_DATA_MAX_FILE_NODES
+            : toss(
+                new InvalidAppEnvironmentError(
+                  'PRUNE_DATA_MAX_FILE_NODES must be greater than zero'
+                )
+              ),
+        async deleteFn(thresholdEntry) {
+          const db = await getDb({ name: 'hscc-api-drive' });
+          const fileNodes = db.collection<InternalNode>('file-nodes');
+          const metaNodes = db.collection<InternalNode>('meta-nodes');
+          const ids = (
+            await fileNodes.find({ _id: { $lte: thresholdEntry._id } }).toArray()
+          ).map((node) => node._id);
+
+          await Promise.all([
+            fileNodes.deleteMany({ _id: { $in: ids } }),
+            metaNodes.updateMany(
+              // * Is this more optimal than a full scan?
+              { contents: { $in: ids } },
+              { $pull: { contents: { $in: ids } } }
             )
-          )
+          ]);
+
+          return ids.length;
+        }
+      },
+      'meta-nodes': {
+        limit:
+          env.PRUNE_DATA_MAX_META_NODES && env.PRUNE_DATA_MAX_META_NODES > 0
+            ? env.PRUNE_DATA_MAX_META_NODES
+            : toss(
+                new InvalidAppEnvironmentError(
+                  'PRUNE_DATA_MAX_META_NODES must be greater than zero'
+                )
+              ),
+        async deleteFn(thresholdEntry) {
+          const db = await getDb({ name: 'hscc-api-drive' });
+          const metaNodes = db.collection<InternalNode>('meta-nodes');
+          const ids = (
+            await metaNodes.find({ _id: { $lte: thresholdEntry._id } }).toArray()
+          ).map((node) => node._id);
+
+          await metaNodes.deleteMany({ _id: { $in: ids } });
+          await metaNodes.updateMany(
+            // * Is this more optimal than a full scan?
+            { contents: { $in: ids } },
+            { $pull: { contents: { $in: ids } } }
+          );
+
+          return ids.length;
+        }
+      },
+      users: {
+        limit:
+          env.PRUNE_DATA_MAX_USERS && env.PRUNE_DATA_MAX_USERS > 0
+            ? env.PRUNE_DATA_MAX_USERS
+            : toss(
+                new InvalidAppEnvironmentError(
+                  'PRUNE_DATA_MAX_USERS must be greater than zero'
+                )
+              ),
+        async deleteFn(thresholdEntry) {
+          const users = (
+            await getDb({ name: 'hscc-api-drive' })
+          ).collection<InternalUser>('users');
+
+          const usernames = (
+            await users.find({ _id: { $lte: thresholdEntry._id } }).toArray()
+          ).map((user) => user.username);
+
+          await Promise.all(usernames.map((username) => deleteUser({ username })));
+          return usernames.length;
+        }
+      }
+    }
   };
 
   debug('limits: %O', limits);
@@ -49,42 +142,64 @@ const getCollectionLimits = (env: ReturnType<typeof getEnv>) => {
  */
 const invoked = async () => {
   try {
-    const limits = getCollectionLimits(getEnv());
-    const db = await getDb({ name: 'root' });
+    const limits = getDbCollectionLimits(getEnv());
 
     await Promise.all(
-      Object.entries(limits).map(async ([collectionName, limitObj]) => {
-        const { limit: limitThreshold, orderBy } =
-          typeof limitObj == 'number'
-            ? { limit: limitObj, orderBy: '_id' }
-            : /* istanbul ignore next */ limitObj;
+      Object.entries(limits).map(async ([dbName, dbLimitsObj]) => {
+        debug(`using db "${dbName}"`);
+        const db = await getDb({ name: dbName });
 
-        const subLog = log.extend(collectionName);
-        const collection = db.collection(collectionName);
-        const total = await collection.countDocuments();
+        await Promise.all(
+          Object.entries(dbLimitsObj).map(async ([collectionName, colLimitsObj]) => {
+            const name = `${dbName}.${collectionName}`;
+            debug(`pruning collection "${name}"`);
 
-        const cursor = collection
-          .find()
-          .sort({ [orderBy]: -1 })
-          .skip(limitThreshold)
-          .limit(1);
+            const {
+              limit: limitThreshold,
+              orderBy = '_id',
+              deleteFn = undefined
+            } = typeof colLimitsObj == 'number'
+              ? { limit: colLimitsObj }
+              : colLimitsObj;
 
-        const thresholdEntry = await cursor.next();
+            const subLog = log.extend(name);
+            const collection = db.collection(collectionName);
+            const total = await collection.countDocuments();
 
-        if (thresholdEntry) {
-          const result = await collection.deleteMany({
-            [orderBy]: { $lte: thresholdEntry[orderBy] }
-          });
+            debug(`sorting ${name} by "${orderBy}"`);
+            debug(`skipping ${limitThreshold} entries"`);
 
-          subLog(`pruned ${result.deletedCount}/${total} "${collectionName}" entries`);
-          debug(`sorted "${collectionName}" by "${orderBy}"`);
-        } else {
-          subLog(
-            `no prunable "${collectionName}" entries (${total} <= ${limitThreshold})`
-          );
-        }
+            const cursor = collection
+              .find()
+              .sort({ [orderBy]: -1 })
+              .skip(limitThreshold)
+              .limit(1);
 
-        await cursor.close();
+            const thresholdEntry = await cursor.next();
+
+            if (thresholdEntry) {
+              let deletedCount: number;
+
+              if (deleteFn) {
+                debug('using custom pruning strategy');
+                deletedCount = await deleteFn(thresholdEntry);
+              } else {
+                debug('using default pruning strategy');
+                deletedCount = (
+                  await collection.deleteMany({
+                    [orderBy]: { $lte: thresholdEntry[orderBy] }
+                  })
+                ).deletedCount;
+              }
+
+              subLog(`pruned ${deletedCount}/${total} ${name} entries`);
+            } else {
+              subLog(`no prunable ${name} entries (${total} <= ${limitThreshold})`);
+            }
+
+            await cursor.close();
+          })
+        );
       })
     );
 
--- test/externals/unit-prune.test.ts
+++ test/externals/unit-prune.test.ts
@@ -1,6 +1,7 @@
 import { setupMemoryServerOverride } from 'multiverse/mongo-test';
 import { getDb } from 'multiverse/mongo-schema';
 import { dummyRootData, useMockDateNow } from 'multiverse/mongo-common';
+import { dummyAppData } from 'testverse/db';
 
 import {
   mockEnvFactory,
@@ -8,20 +9,36 @@ import {
   withMockedOutput
 } from 'testverse/setup';
 
-import type { InternalRequestLogEntry } from 'multiverse/next-log';
+import { TrialError } from 'named-app-errors';
+import { InternalFileNode, InternalMetaNode } from 'universe/backend/db';
+
+// * Follow the steps (below) to tailor these tests to this specific project 😉
 
 // ? Ensure the isolated external picks up the memory server override
 jest.mock('multiverse/mongo-schema', () => {
   return jest.requireActual('multiverse/mongo-schema');
 });
 
-const testCollections = ['request-log', 'limited-log'];
+const testCollectionsMap = {
+  'root.request-log': dummyRootData['request-log'].length,
+  'root.limited-log': dummyRootData['limited-log'].length,
+  // * Step 1: Add new collections here w/ keys of the form: database.collection
+  'hscc-api-drive.file-nodes': dummyAppData['file-nodes'].length,
+  'hscc-api-drive.meta-nodes': dummyAppData['meta-nodes'].length,
+  'hscc-api-drive.users': dummyAppData['users'].length
+};
 
 const withMockedEnv = mockEnvFactory({
   NODE_ENV: 'test',
   PRUNE_DATA_MAX_LOGS: '200000',
-  PRUNE_DATA_MAX_BANNED: '100000'
+  PRUNE_DATA_MAX_BANNED: '100000',
+  // * Step 2: Add new env var default values here
+  PRUNE_DATA_MAX_FILE_NODES: '10000',
+  PRUNE_DATA_MAX_META_NODES: '100000',
+  PRUNE_DATA_MAX_USERS: '100000'
 });
+
+const testCollections = Object.keys(testCollectionsMap);
 
 const importPruneData = protectedImportFactory<
   typeof import('externals/prune-data').default
@@ -33,27 +50,44 @@ const importPruneData = protectedImportFactory<
 setupMemoryServerOverride();
 useMockDateNow();
 
-const countCollection = async (collections: string | string[]) => {
+/**
+ * Accepts one or more database and collection names in the form
+ * `database.collection` and returns their size.
+ */
+async function countCollection(collections: string): Promise<number>;
+async function countCollection(collections: string[]): Promise<Record<string, number>>;
+async function countCollection(
+  collections: string | string[]
+): Promise<number | Record<string, number>> {
+  const targetCollections = [collections].flat();
   const result = Object.assign(
     {},
     ...(await Promise.all(
-      [collections].flat().map((collection) =>
-        getDb({ name: 'root' }).then((db) =>
-          db
-            .collection<InternalRequestLogEntry>(collection)
-            .countDocuments()
-            .then((count) => ({ [collection]: count }))
-        )
-      )
+      targetCollections.map(async (dbCollection) => {
+        const [dbName, ...rawCollectionName] = dbCollection.split('.');
+
+        if (!dbName || rawCollectionName.length != 1) {
+          throw new TrialError(`invalid input "${dbCollection}" to countCollection`);
+        }
+
+        return (await getDb({ name: dbName }))
+          .collection(rawCollectionName[0])
+          .countDocuments()
+          .then((count) => ({ [dbCollection]: count }));
+      })
     ))
   );
 
-  return Object.keys(result).length == 1
-    ? (result[collections.toString()] as number)
-    : (result as Record<string, number>);
-};
+  const resultLength = Object.keys(result).length;
 
-it('becomes verbose when no DEBUG environment variable and NODE_ENV is not test', async () => {
+  if (resultLength != targetCollections.length) {
+    throw new TrialError('invalid output from countCollection');
+  }
+
+  return resultLength == 1 ? result[collections.toString()] : result;
+}
+
+it('becomes verbose when no DEBUG environment variable set and NODE_ENV is not test', async () => {
   expect.hasAssertions();
 
   await withMockedOutput(async ({ infoSpy }) => {
@@ -75,6 +109,19 @@ it('becomes verbose when no DEBUG environment variable and NODE_ENV is not test'
 it('rejects on bad environment', async () => {
   expect.hasAssertions();
 
+  // * Step 3: Add new env vars emptiness tests below
+
+  // ? Remember that withMockedEnv is the result of calling a factory function
+  // ? with all the PRUNE_DATA_MAX_X env vars already defined.
+
+  await withMockedEnv(() => importPruneData({ expectedExitCode: 2 }), {
+    PRUNE_DATA_MAX_LOGS: '',
+    PRUNE_DATA_MAX_BANNED: '',
+    PRUNE_DATA_MAX_FILE_NODES: '',
+    PRUNE_DATA_MAX_META_NODES: '',
+    PRUNE_DATA_MAX_USERS: ''
+  });
+
   await withMockedEnv(() => importPruneData({ expectedExitCode: 2 }), {
     PRUNE_DATA_MAX_LOGS: ''
   });
@@ -82,24 +129,52 @@ it('rejects on bad environment', async () => {
   await withMockedEnv(() => importPruneData({ expectedExitCode: 2 }), {
     PRUNE_DATA_MAX_BANNED: ''
   });
+
+  await withMockedEnv(() => importPruneData({ expectedExitCode: 2 }), {
+    PRUNE_DATA_MAX_FILE_NODES: ''
+  });
+
+  await withMockedEnv(() => importPruneData({ expectedExitCode: 2 }), {
+    PRUNE_DATA_MAX_META_NODES: ''
+  });
+
+  await withMockedEnv(() => importPruneData({ expectedExitCode: 2 }), {
+    PRUNE_DATA_MAX_USERS: ''
+  });
 });
 
 it('ensures at most PRUNE_DATA_MAX_X entries exist', async () => {
   expect.hasAssertions();
 
-  await expect(countCollection(testCollections)).resolves.toStrictEqual({
-    'request-log': dummyRootData['request-log'].length,
-    'limited-log': dummyRootData['limited-log'].length
-  });
+  await expect(countCollection(testCollections)).resolves.toStrictEqual(
+    testCollectionsMap
+  );
+
+  // * Step 4: Add new env vars low-prune-threshold tests below
+
+  const db = await getDb({ name: 'hscc-api-drive' });
+
+  await db
+    .collection<InternalFileNode>('file-nodes')
+    .updateOne(
+      { _id: dummyAppData['file-nodes'][2]._id },
+      { $set: { permissions: { User3: 'view', User1: 'edit' } } }
+    );
 
   await withMockedEnv(importPruneData, {
     PRUNE_DATA_MAX_LOGS: '10',
-    PRUNE_DATA_MAX_BANNED: '2'
+    PRUNE_DATA_MAX_BANNED: '2',
+    PRUNE_DATA_MAX_FILE_NODES: '1',
+    PRUNE_DATA_MAX_META_NODES: '1',
+    PRUNE_DATA_MAX_USERS: '1'
   });
 
   await expect(countCollection(testCollections)).resolves.toStrictEqual({
-    'request-log': 10,
-    'limited-log': 2
+    'root.request-log': 10,
+    'root.limited-log': 2,
+    'hscc-api-drive.file-nodes': 1,
+    'hscc-api-drive.meta-nodes': 1,
+    'hscc-api-drive.users': 1
   });
 
   await withMockedEnv(importPruneData, {
@@ -108,26 +183,49 @@ it('ensures at most PRUNE_DATA_MAX_X entries exist', async () => {
   });
 
   await expect(countCollection(testCollections)).resolves.toStrictEqual({
-    'request-log': 1,
-    'limited-log': 1
+    'root.request-log': 1,
+    'root.limited-log': 1,
+    'hscc-api-drive.file-nodes': 1,
+    'hscc-api-drive.meta-nodes': 1,
+    'hscc-api-drive.users': 1
   });
+
+  await expect(
+    db
+      .collection<InternalFileNode>('file-nodes')
+      .findOne(
+        { _id: dummyAppData['file-nodes'][2]._id },
+        { projection: { _id: false, permissions: true } }
+      )
+  ).resolves.toStrictEqual({ permissions: { User3: 'view' } });
+
+  await expect(
+    db
+      .collection<InternalMetaNode>('meta-nodes')
+      .findOne(
+        { _id: dummyAppData['meta-nodes'][0]._id },
+        { projection: { _id: false, contents: true } }
+      )
+  ).resolves.toStrictEqual({ contents: [] });
 });
 
 it('only deletes entries if necessary', async () => {
   expect.hasAssertions();
 
-  await expect(countCollection(testCollections)).resolves.toStrictEqual({
-    'request-log': dummyRootData['request-log'].length,
-    'limited-log': dummyRootData['limited-log'].length
-  });
+  await expect(countCollection(testCollections)).resolves.toStrictEqual(
+    testCollectionsMap
+  );
 
   await withMockedEnv(importPruneData, {
     PRUNE_DATA_MAX_LOGS: '100',
-    PRUNE_DATA_MAX_BANNED: '100'
+    PRUNE_DATA_MAX_BANNED: '100',
+    // * Step 5: Add new env vars high-prune-threshold values here
+    PRUNE_DATA_MAX_FILE_NODES: '100',
+    PRUNE_DATA_MAX_META_NODES: '100',
+    PRUNE_DATA_MAX_USERS: '100'
   });
 
-  await expect(countCollection(testCollections)).resolves.toStrictEqual({
-    'request-log': dummyRootData['request-log'].length,
-    'limited-log': dummyRootData['limited-log'].length
-  });
+  await expect(countCollection(testCollections)).resolves.toStrictEqual(
+    testCollectionsMap
+  );
 });
